c2dee788d2c4c4792afa9f283af536dc55bc5164
jriley15 <jordanr3@live.com>
2019-09-07 13:02:37 -0700

    spa dependencies

--- spa/src/App.js
+++ spa/src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { Provider } from "react-redux";
+import store from "./store/configureStore";
+import Home from "./pages/Home";
+import Template from "./containers/Template";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Template />
+    </Provider>
   );
 }
-
-export default App;
---
+++ spa/src/containers/Template.js
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+import Routes from "./Routes";
+import NavBar from "../components/Navbar";
+
+export default function Template() {
+  return (
+    <Routes>
+      <NavBar />
+    </Routes>
+  );
+}
